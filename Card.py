c872db3c7c6d5e9cf386d599796a2b14477b8c72
gabrielbckr <gabriel@etac.lan>
2018-11-27 11:19:02 -0200

    iniciando a classe cartas

---
+++ Card.py
@@ -0,0 +1,14 @@
+class Card:
+    def __init__(self, cc, nn): # cc -> cor || nn -> number 
+        if cc != 'G' and cc != 'R' and cc != 'B' and cc != 'Y':
+            # dar ruim
+        
+        self.color  = cc
+        self.number = string(nn)
+    def isYellow(self):
+        return self.color == 'Y'
+    def isGreen(self):
+    def isRed(self):
+    def isBlue(self):
+    def isJoker(self):
+    

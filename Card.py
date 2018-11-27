8a2968ee079dd9c402f267261e3c2e26c0360c19
gabrielbckr <gabriel@etac.lan>
2018-11-27 12:31:07 -0200

    mudei a ordem de CorNumero para NumeroCor

--- Card.py
+++ Card.py
@@ -1,14 +1,19 @@
 class Card:
-    def __init__(self, cc, nn): # cc -> cor || nn -> number 
+    def __init__(self, nn, cc): # cc -> cor || nn -> number 
         if cc != 'G' and cc != 'R' and cc != 'B' and cc != 'Y':
             # dar ruim
-        
+            print("so pra n ficar o erro ")
         self.color  = cc
-        self.number = string(nn)
+        self.number = str(nn)
     def isYellow(self):
         return self.color == 'Y'
     def isGreen(self):
+        return self.color == 'G'
     def isRed(self):
+        return self.color == 'R'
     def isBlue(self):
+        return self.color == 'Y'
     def isJoker(self):
-    
+        return self.number == '#'
+    def isValid(self, otherCard):
+        return otherCard.number == self.number or otherCard.color == self.color

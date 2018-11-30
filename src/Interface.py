649aa53030220c4bedd275686a0617273e46f0a1
gabrielbckr <gabriel@etac.lan>
2018-11-30 16:09:14 -0200

    Corrections so Interface wont sotore cards

--- src/Interface.py
+++ src/Interface.py
@@ -2,13 +2,10 @@
 from Card import Card
 
 class Interface:
-    hand = list()
     def __init__(self):
         self.shouldStop = 0 
         self.shouldPost = 0 
         self.answer = 0 
-    def addCard(self, c):
-        self.hand.append(c)
     def solve(self, cmd):
         print(cmd)
         self.shouldStop = 0 
@@ -16,6 +13,3 @@ class Interface:
         self.answer = 0 
         if cmd == "refuse":
             self.shouldStop = True
-    def showHand(self):
-        for cs in self.hand:
-            print(cs)

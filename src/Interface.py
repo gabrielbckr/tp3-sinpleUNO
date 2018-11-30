fa37dbca35f53ed03452b9d86224fc703ba263d7
gabrielbckr <gabriel@etac.lan>
2018-11-30 15:52:08 -0200

    Interface notifiers

--- src/Interface.py
+++ src/Interface.py
@@ -4,11 +4,18 @@ from Card import Card
 class Interface:
     hand = list()
     def __init__(self):
-        print("Hello")
+        self.shouldStop = 0 
+        self.shouldPost = 0 
+        self.answer = 0 
     def addCard(self, c):
         self.hand.append(c)
     def solve(self, cmd):
         print(cmd)
+        self.shouldStop = 0 
+        self.shouldPost = 0 
+        self.answer = 0 
+        if cmd == "refuse":
+            self.shouldStop = True
     def showHand(self):
         for cs in self.hand:
             print(cs)

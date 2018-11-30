efbb31947b35a0eec3cab595a00db1d5eb39fa68
gabrielbckr <gabriel@etac.lan>
2018-11-30 13:57:11 -0200

    server uses class socketThread

--- src/servidor.py
+++ src/servidor.py
@@ -1,41 +1,9 @@
 import sys
 import socket as sock
 import threading
-import queue
 import time
+import socketThread
 
-class sockThread (threading.Thread):
-    def __init__(self, threadID, name, skt, q):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.s = skt
-      self.rcvQ = q
-      self.fstop = False
-    def run(self):
-        while not self.fstop:
-            if not self.rcvQ.empty():
-                msg = self.rcvQ.get()
-                self.s.send(msg.encode())
-    def stop(self):
-        self.fstop = True
-    def post(self, m):
-        self.rcvQ.put(m)
-
-class refuseConnection (threading.Thread):
-    def __init__(self, sr):
-        threading.Thread.__init__(self)
-        self.s = sr
-        self.fstop = False
-    def run(self):
-        while not self.fstop:
-            self.s.listen(numPlayers)
-            r , rr = s.accept()
-            r.send("refuse".encode())
-            del(rr)
-            r.close()
-    def stop(self):
-        self.fstop = True
 
 print (len(sys.argv))
 if (len(sys.argv))<2:
@@ -53,13 +21,12 @@ s.listen(numPlayers)
 
 while ii < numPlayers:
     cl_sock, cl_addr = s.accept()
-    q = queue.Queue(100)
-    player = sockThread(ii, "player"+str(ii), cl_sock, q)
+    player = socketThread.sockThread(cl_sock, ii, "player"+str(ii))
     playerThreads.append(player)
     player.start()
     ii+=1
 
-refuser = refuseConnection(s)
+refuser = socketThread.refuseConnection(s)
 refuser.start()
 
 jj = 0

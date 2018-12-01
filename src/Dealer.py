0f792c45d6d0c1ab6ca2627c0f710869d014bd71
gabrielbckr <gabriel@etac.lan>
2018-12-01 13:57:03 -0200

    P, I, S, E messages; giveCards

--- src/Dealer.py
+++ src/Dealer.py
@@ -6,11 +6,17 @@ class Dealer:
         self.players = list()
         self.playerhand = list(list())
         self.EndGame = False
-        self.currentPlayer = 0
+        # self.currentPlayer
+        # self.previousPlayer 
+        self.deck.shuffle()
+        # self.currentCard
     def addPlayer(self, p):
         self.players.append(p)
     def rungame(self):
-        while self.EndGame:
+        self.previousPlayer = self.players[-1]
+        self.currentPlayer = self.players[0]
+        self.currentCard = self.deck.get1stCard()
+        while not self.EndGame:
             self.round()
     def round(self):
         # Avisa de quem é a vez
@@ -21,11 +27,37 @@ class Dealer:
         self.players[self.currentPlayer].post(message)
         # Checa estado do Jogo
         # Envia mensagem S para todos os outros jogadores
-        self.Smessage(self.currentPlayer)
+        self.Smessage(self.previousPlayer.name, self.currentPlayer.name, self.currentPlayer)
         # Muda de jogador
     def Pmessage(self, p):
-        print("")
-    def Smessage(self, p):
-        print("")
+        if type(p) is int:
+            p = self.players[p] 
+        string = self.currentCard.number+self.currentCard.color+" "+p.getHand()+" "
+        for player in self.players:
+            if player is not p:
+                string+=str(player.name)+" "+str(player.numCards)
+        return string
+    def Smessage(self, nome1, nome2, p):
+        if type(p) is int:
+            p = self.players[p] 
+        string=nome1+" "+self.currentCard.number+self.currentCard.color+" "+nome2+" "+p.getHand()+" "
+        for player in self.players:
+            if player is not p:
+                string+=str(player.name)+" "+str(player.numCards)
+        return "S "+string
     def Imessage(self, p):
-        print("")
+        if type(p) is int:
+            p = self.players[p] 
+        string = self.currentCard+" "+p.getHand()
+        return "I "+string
+    def Emessage(self, p):
+        if type(p) is int:
+            p = self.players[p] 
+        return "E "+p.name
+    def giveCards(self):
+        numofcards = 7
+        for i in range(numofcards):
+            i = i # linha inutil pq o vscode fica enchendo  osaco
+            for player in self.players:
+                player.addCard(self.deck.get1stCard())
+    

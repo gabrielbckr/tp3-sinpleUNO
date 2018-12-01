4cf8be113f6372c9c0602c519c43cd77c920c113
gabrielbckr <gabriel@etac.lan>
2018-12-01 13:52:36 -0200

    test script accepts argument number os clients and runs in generic gnulinux distros

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 RANDPORT=$(python -c 'from random import randint; print(randint(2500, 4500));')
+NUM=2
+if [ $# != 0 ] 
+then
+    NUM="$1"
+fi
 echo "PORT IS $RANDPORT"
-	
-gnome-terminal -e "python	src/servidor.py $RANDPORT 1"
+echo "Deploy server"
+x-terminal-emulator -e "python	src/servidor.py $RANDPORT $NUM"
 sleep 1
-gnome-terminal -e  "python src/cliente.py local $RANDPORT"
-sleep 1
+ii=0
+while [ "$ii" -lt "$NUM" ]
+    do
+        echo "Deploy client `expr $ii + 1` of $NUM"
+        x-terminal-emulator -e  "python src/cliente.py local $RANDPORT"
+        sleep 1
+        ii=`expr $ii + 1`
+done
 python src/cliente.py local $RANDPORT
